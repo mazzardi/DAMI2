a5a502abe25146a2fb0b16b6dfcb6107b80831f7
ljung <ljungmanjimmy@hotmail.com>
2020-12-08 20:00:21 +0100

    - final_final is the final version of code. - cleaned up

--- models/UnbalancedBagger.py
+++ models/UnbalancedBagger.py
@@ -3,7 +3,8 @@ from sklearn.tree import DecisionTreeClassifier
 import random
 from scipy.stats import mode
 
-def unbalanced_bagger(x, y, M = 1, sampling_techniques = None, base_estimator = None, random_seed = None):
+
+def unbalanced_bagger(x, y, M=1, sampling_techniques=None, base_estimator=None, random_seed=None):
     if sampling_techniques is None:
         sampling_techniques = []
     if base_estimator is None:
@@ -14,20 +15,27 @@ def unbalanced_bagger(x, y, M = 1, sampling_techniques = None, base_estimator = 
 
     base_learners = []
     for i in range(M):
-
         rnd = np.random.choice(x.shape[0], size=x.shape[0])
-        x_bootstrap = x.iloc[rnd,:]
+        x_bootstrap = x.iloc[rnd, :]
         y_bootstrap = y.iloc[rnd]
 
-        technique = random.randint(0,len(sampling_techniques)-1)
-        x_sample, y_sample = sampling_techniques[technique].fit_sample(x_bootstrap,y_bootstrap)
+        technique = random.randint(0, len(sampling_techniques) - 1)
+        x_sample, y_sample = sampling_techniques[technique].fit_sample(x_bootstrap, y_bootstrap)
 
-        base_estimator.fit(x_sample,y_sample)
+        base_estimator.fit(x_sample, y_sample)
         base_learners.append(base_estimator)
 
     def predict(x):
         predictions = np.zeros([x.shape[0], len(base_learners)])
         for i in range(len(base_learners)):
-            predictions[:,i] = base_learners[i].predict(x)
+            predictions[:, i] = base_learners[i].predict(x)
         return mode(predictions, axis=1)[0]
-    return predict
+
+    def predict_proba(x):
+        n_classes = len(base_learners[0].classes_)
+        proba = np.zeros([x.shape[0], n_classes])
+        for i, base_learner in enumerate(base_learners):
+            proba += base_learner.predict_proba(x)
+        return proba / len(base_learners)
+
+    return predict, predict_proba
--- models/UnbalancedBaggerNoSampling.py
+++ models/UnbalancedBaggerNoSampling.py
@@ -2,7 +2,8 @@ import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from scipy.stats import mode
 
-def unbalanced_bagger_no_sampling(x, y, M = 1, base_estimator = None, random_seed = None):
+
+def unbalanced_bagger_no_sampling(x, y, M=1, base_estimator=None, random_seed=None):
     if base_estimator is None:
         if random_seed is None:
             base_estimator = DecisionTreeClassifier()
@@ -12,14 +13,22 @@ def unbalanced_bagger_no_sampling(x, y, M = 1, base_estimator = None, random_see
     base_learners = []
     for i in range(M):
         rnd = np.random.choice(x.shape[0], size=x.shape[0])
-        x_bootstrap = x.iloc[rnd,:]
+        x_bootstrap = x.iloc[rnd, :]
         y_bootstrap = y.iloc[rnd]
-        base_estimator.fit(x_bootstrap,y_bootstrap)
+        base_estimator.fit(x_bootstrap, y_bootstrap)
         base_learners.append(base_estimator)
 
     def predict(x):
         predictions = np.zeros([x.shape[0], len(base_learners)])
         for i in range(len(base_learners)):
-            predictions[:,i] = base_learners[i].predict(x)
+            predictions[:, i] = base_learners[i].predict(x)
         return mode(predictions, axis=1)[0]
-    return predict
+
+    def predict_proba(x):
+        n_classes = len(base_learners[0].classes_)
+        proba = np.zeros([x.shape[0], n_classes])
+        for i, base_learner in enumerate(base_learners):
+            proba += base_learner.predict_proba(x)
+        return proba / len(base_learners)
+
+    return predict, predict_proba
